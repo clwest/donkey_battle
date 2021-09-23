76cb2050dbe5cc5d87dddb9dedefe5e56808caa0
clwest <westremarketing@gmail.com>
2021-09-22 20:09:34 -0600

    all base donkey's minted to opensea testnet

--- scripts/donkey_battle/set_tokenuri.py
+++ scripts/donkey_battle/set_tokenuri.py
@@ -3,11 +3,11 @@ from scripts.helpful_scripts import get_name, get_account, OPENSEA_URL
 
 
 donkey_metadata_dic = {
-# "BUSTA": "https://ipfs.io/ipfs/QmdHoMucYyNZunrwb9C6pTtaxKHtbt21NZ6k3Z5Zmbwfqz?filename=0-BUSTA.json",
-# "REBEL": "https://ipfs.io/ipfs/QmbZjdhuwKcArzV1JTPFh122AscRZvt9RYiqMLU3KowHnz?filename=1-REBEL.json",
+"BUSTA": "https://ipfs.io/ipfs/QmdHoMucYyNZunrwb9C6pTtaxKHtbt21NZ6k3Z5Zmbwfqz?filename=0-BUSTA.json",
+"REBEL": "https://ipfs.io/ipfs/QmU6HSBhQ3JXwRpVH6r7XmmSRCdoayPdsKhQkYW6t9NAyW?filename=2-REBEL.json",
 "JETHRO": "https://ipfs.io/ipfs/QmfDR6vLkwoiRmjtm1W8p5SWRM6WnJcKTKpbE4EY3Ptrjq?filename=0-JETHRO.json",
-# "ROSCOE": "https://ipfs.io/ipfs/QmTA1sqLdsVgV91oiST2DogK4PLjfwq1v4dM3esUSvk4BA?filename=3-ROSCOE.json",
-# "TWEAK": "https://ipfs.io/ipfs/QmefsrvJ5h8MZ42FxHq4FebG5x7c3d3XRD5tegreTbbLVx?filename=4-TWEAK.json",
+"ROSCOE": "https://ipfs.io/ipfs/QmTPnC5jksNDyYnPbpZRDzMREg3PFxFqGEYuSh1KNhhyLH?filename=1-ROSCOE.json",
+"TWEAK": "https://ipfs.io/ipfs/Qmetcvp6978S9KJLX4EzSiKukEb2m7Hzzv3XwrVxbsX49L?filename=0-TWEAK.json",
   
 }
 
