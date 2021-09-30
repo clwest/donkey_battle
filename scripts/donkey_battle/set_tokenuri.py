41ac2c8efdee7e33af00cd09a09aedb1204e59cd
clwest <westremarketing@gmail.com>
2021-09-30 10:32:48 -0600

    cleaned up some commented out sections

--- scripts/donkey_battle/set_tokenuri.py
+++ scripts/donkey_battle/set_tokenuri.py
@@ -2,16 +2,6 @@ from brownie import network, BattleDonkeyz
 from scripts.helpful_scripts import get_name, get_account, OPENSEA_URL
 from metadata.donkey_metadata import donkey_metadata_dic
 
-
-# donkey_metadata_dic = {
-# "BUSTA": "https://ipfs.io/ipfs/QmdHoMucYyNZunrwb9C6pTtaxKHtbt21NZ6k3Z5Zmbwfqz?filename=0-BUSTA.json",
-# "REBEL": "https://ipfs.io/ipfs/QmU6HSBhQ3JXwRpVH6r7XmmSRCdoayPdsKhQkYW6t9NAyW?filename=2-REBEL.json",
-# "JETHRO": "https://ipfs.io/ipfs/QmfDR6vLkwoiRmjtm1W8p5SWRM6WnJcKTKpbE4EY3Ptrjq?filename=0-JETHRO.json",
-# "ROSCOE": "https://ipfs.io/ipfs/QmTPnC5jksNDyYnPbpZRDzMREg3PFxFqGEYuSh1KNhhyLH?filename=1-ROSCOE.json",
-# "TWEAK": "https://ipfs.io/ipfs/Qmetcvp6978S9KJLX4EzSiKukEb2m7Hzzv3XwrVxbsX49L?filename=0-TWEAK.json",
-# }
-# 
-
 def main():
   print(f"Working on {network.show_active()}")
   donkey_warrior = BattleDonkeyz[-1]
